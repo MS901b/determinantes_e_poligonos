baa75343e5ec0f4c73d1011326c934b29b64b9cc
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-08-29 21:45:21 -0300

    Aplicado revisão do software

--- scripts/atividade1_conteudo.js
+++ scripts/atividade1_conteudo.js
@@ -1,5 +1,5 @@
 /*
-	Padronização do ID: 
+	Padronização do ID:
 		- [['p/parte','q/questao','/item'],'_'] vai gerar um id do tipo p1_q2_1
 		- [['p/parte','q/questao','/itemletra'],'_'] vai gerar um id do tipo p1_q2_a
 	Palavras-chave: questao, parte, item, itemletra, subitem
@@ -12,19 +12,19 @@ var IdPadrao = [['parte/parte','q/questao','/itemletra','/subitem'],'_'];
 
 /*
 	Questoes
-	
+
 	Aqui ficam concentrados todos os conteudos das questões da atividade!
 	Veja que está separado por Parte/Questão/Item
-	
+
 	ATENÇÃO: Cada tipo possui um formato de entrada característico.
 */
 
-var Questoes = 
+var Questoes =
 [
 	{//Parte 1
 		parte1_q1: //Questão 1
 		{
-			itens: 
+			itens:
 			[
 				{//A
 					tipo: 'input',
@@ -35,7 +35,7 @@ var Questoes =
 					enunciado: 'Qual é o valor do determinante da matriz abaixo, cuja primeira linha é formada pelas coordenadas de P e a segunda pelas coordenadas de Q?',
 					depois_enunciado: '<div class="matriz margem_questao"><div class="supe_esq"></div><div class="supe_dir"></div><span id="parte1_q1_a_11">Xp</span><span id="parte1_q1_a_12">Yp</span><br class="limpador" /><span id="parte1_q1_a_21">Xq</span><span id="parte1_q1_a_22">Yq</span><br class="limpador" /><div class="esquerda inferior infe_esq"></div><div class="direita inferior infe_dir"></div></div><br class="limpador" />',
 					antes: 'Det =',
-					msgErro: 'Lembre-se que o valor do determinante é dado por: a<sub style="color:red">11</sub>*a<sub style="color:red">22</sub> - a<sub style="color:red">21</sub>*a<sub style="color:red">12</sub>'
+					msgErro: 'Lembre-se que o valor do determinante é dado por: X<sub style="color:red">p</sub>*Y<sub style="color:red">q</sub> - X<sub style="color:red">q</sub>*Y<sub style="color:red">p</sub>'
 				},
 				{//B
 					tipo: 'input',
@@ -44,15 +44,15 @@ var Questoes =
 					dependente: false,
 					associado: false,
 					enunciado: 'Qual é o valor do determinante da matriz abaixo, cuja primeira linha é formada pelas coordenadas de Q e a segunda pelas coordenadas de P?',
-					depois_enunciado: '<div class="matriz margem_questao"><div class="supe_esq"></div><div class="supe_dir"></div><span id="parte1_q1_b_11">Xq</span><span id="parte1_q1_b_12">Yq</span><br class="limpador" /><span id="parte1_q1_b_21">Xp</span><span id="parte1_q1_b_22">Yp</span><br class="limpador" /><div class="esquerda inferior infe_esq"></div><div class="direita inferior infe_dir"></div></div><br class="limpador" />', 
+					depois_enunciado: '<div class="matriz margem_questao"><div class="supe_esq"></div><div class="supe_dir"></div><span id="parte1_q1_b_11">Xq</span><span id="parte1_q1_b_12">Yq</span><br class="limpador" /><span id="parte1_q1_b_21">Xp</span><span id="parte1_q1_b_22">Yp</span><br class="limpador" /><div class="esquerda inferior infe_esq"></div><div class="direita inferior infe_dir"></div></div><br class="limpador" />',
 					antes: 'Det =',
-					msgErro: 'Lembre-se que o valor do determinante é dado por: a<sub style="color:red">11</sub>*a<sub style="color:red">22</sub> - a<sub style="color:red">21</sub>*a<sub style="color:red">12</sub>'
+					msgErro: 'Lembre-se que o valor do determinante é dado por: X<sub style="color:red">p</sub>*Y<sub style="color:red">q</sub> - X<sub style="color:red">q</sub>*Y<sub style="color:red">p</sub>'
 				}
 			]
 		},
 		parte1_q2:
 		{
-			itens: 
+			itens:
 			[
 				{//A
 					tipo: 'multipla_escolha',
@@ -65,7 +65,7 @@ var Questoes =
 						{value: '1', label: '<span id="parte1_q2_a3"></span>'},
 						{value: '0', label: '<span id="parte1_q2_a4"></span>'}
 					],
-					msgErro: 'Lembre-se que a área deste triângulo é dada por: (a<sub_red>11</sub_red>*a<sub_red>22</sub_red> - a<sub_red>21</sub_red>*a<sub_red>12</sub_red>)/2'
+					msgErro: 'Lembre-se que a área é uma quantidade, logo, deverá ser positiva e seu valor é dado pelo determinante: X<sub style="color:red">p</sub>*Y<sub style="color:red">q</sub> - X<sub style="color:red">q</sub>*Y<sub style="color:red">p</sub>'
 				}
 			]
 		}
--- scripts/atividade3_conteudo.js
+++ scripts/atividade3_conteudo.js
@@ -1,30 +1,30 @@
 /*
-	Padronização do ID: 
+	Padronização do ID:
 		- [['p/parte','q/questao','/item'],'_'] vai gerar um id do tipo p1_q2_1
 		- [['p/parte','q/questao','/itemletra'],'_'] vai gerar um id do tipo p1_q2_a
 	Palavras-chave: questao, parte, item, itemletra, subitem
 	Devem ser precedidas de uma barra '/'.
 	A palavra-chave subitem será usada somente em questões com mais de um campo
 */
- 
+
 var IdPadrao = [['parte/parte','q/questao','/itemletra','/subitem'],'_'];
 var Partes = ['1','2','3'];
 var nomeSoft = 'area_poligonos';
 /*
 	Questoes
-	
+
 	Aqui ficam concentrados todos os conteudos das questões da atividade!
 	Veja que está separado por Parte/Questão/Item
-	
+
 	ATENÇÃO: Cada tipo possui um formato de entrada característico.
 */
 
-var Questoes = 
+var Questoes =
 [
 	{
 		parte4_q1: //Questão 1
 		{
-			itens: 
+			itens:
 			[
 				{//A
 					tipo: 'instrucao',
@@ -38,8 +38,8 @@ var Questoes =
 		},
 		parte4_q2: //Questão 1
 		{
-			enunciadoGeral:'Dê o percurso que resulta na área de cada um dos triângulos.',
-			itens: 
+			enunciadoGeral:'Dê o percurso dos vértices de cada um dos triângulos.',
+			itens:
 			[
 				{//A
 					tipo: 'input',
@@ -82,7 +82,7 @@ var Questoes =
 	{
 		parte4_q3: //Questão 1
 		{
-			itens: 
+			itens:
 			[
 				{//A
 					tipo: 'input',
